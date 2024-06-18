ce059bf1e7918229733c88e29b434bc8e16612d3
rayncapitao <ryancapitao.mm.123@gmail.com>
2024-06-18 18:37:01 -0300

    updated: more stored procedures

--- procedures.sql
+++ procedures.sql
@@ -2,18 +2,36 @@ USE capulus;
 
 -- Procedure para adicionar uma nova pessoa no banco de dados
 DELIMITER //
-CREATE PROCEDURE addPessoa(
-	nome VARCHAR(25), 
-	sobrenome VARCHAR(75), 
-	email VARCHAR(100), 
-	cpf VARCHAR(14)
-)
+CREATE PROCEDURE addPessoa(p_nome VARCHAR(25), p_sobrenome VARCHAR(75), p_email VARCHAR(100), p_cpf VARCHAR(14))
 BEGIN
-	INSERT INTO pessoas (nome, sobrenome, email, cpf) 
-		VALUES (nome, sobrenome, email, cpf);
+	INSERT INTO pessoas (p_nome, p_sobrenome, p_email, p_cpf) 
+		VALUES (p_nome, p_sobrenome, p_email, p_cpf);
 END //
 DELIMITER ;
 
 CALL addPessoa('João', 'Marques da Silva', 'JoaoMarquesDaSilva@gmail.com', '872.234.658-10');
 DROP PROCEDURE addPessoa;
 SELECT * FROM pessoas;
+
+-- Procedure para listar os produtos por categoria
+DELIMITER $$
+CREATE PROCEDURE listProductsByCategoryID(p_id_categoria INT)
+BEGIN
+	DECLARE productCount INT;
+    
+    SELECT COUNT(id_produto) INTO productCount 
+		FROM produtos 
+        WHERE id_categoria = p_id_categoria;
+    
+    IF productCount = 0 THEN
+		SELECT "Não há produtos na categoria informada." AS Erro;
+	ELSE
+		SELECT * 
+			FROM produtos 
+			WHERE id_categoria = p_id_categoria;
+    END IF;
+END $$
+DELIMITER ;
+
+CALL listProductsByCategoryID(2);
+DROP PROCEDURE listProductsByCategoryID;
